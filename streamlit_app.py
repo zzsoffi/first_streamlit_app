0676fe6452ff88d62c69ff9c5de3afb62aa57959
zzsoffi <138692677+zzsoffi@users.noreply.github.com>
2023-07-06 13:26:23 +0200

    Update streamlit_app.py
    emojis
    new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothe')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothe')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
